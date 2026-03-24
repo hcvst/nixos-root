71ccc499fa341d612ea73b68ed4c97f0eaee3a95
HC <hc@vst.io>
2026-03-24 19:44:01 +0000

    expand ulthc config

--- hosts/ulthc/default.nix
+++ hosts/ulthc/default.nix
@@ -3,26 +3,67 @@
   lib,
   pkgs,
   ...
-} @ args:
+}@args:
 {
-  imports = [ ];
+  imports = [
+    ../common/users/hcvst.nix
+  ];
+
   boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
-  services.openssh.enable = true;
 
-  environment.systemPackages = map lib.lowPrio [
-    pkgs.curl
-    pkgs.gitMinimal
+  networking = {
+    hostName = "ulthc";
+    useDHCP = true;
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
   ];
 
-  users.users.root.openssh.authorizedKeys.keys =
-  [
+  nix.settings.trusted-users = [ "hcvst" ]; # required for devenv cache
+
+  environment.localBinInPath = true;
+
+  fonts.packages = with pkgs; [
+    # nerd-fonts.fira-code
+    # nerd-fonts.droid-sans-mono
+  ];
+
+  programs.nh = {
+    enable = true;
+    flake = "/etc/nixos";
+  };
+
+  programs.nix-ld.enable = true;
+
+  services.cron.enable = true;
+  services.openssh.enable = true;
+
+  environment.systemPackages = with pkgs; [
+      bat
+    #   devenv
+    #   eza
+    #   fastfetch
+    #   fzf
+      gh
+    #   glow
+      helix
+    #   lld
+    #   mdcat
+      tree
+    #   wget
+    #   zk
+    # nvim
+    nixfmt
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPAoIcEYv9mjFujy7fWjMLFt27oGBCNufUHRjiY6hAzZ"
-  ]; 
+  ];
 
   system.stateVersion = "25.11";
 }
