25b8a8a972fe9f2b73aa2d085df71f3055c9756d
hcvst <hc@vst.io>
2026-04-21 16:43:57 +0200

    enable sops and pet for generic
    enable sops and pet for generic
    
    enable sops and pet for generic
    
    enable sops and pet for generic

--- home/hcvst/generic.nix
+++ home/hcvst/generic.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ config, lib, ... }:
 {
   imports = [
     ./global
+    ../common/optional/sops.nix
+    ../common/optional/pet.nix
   ];
 
   targets.genericLinux.enable = true;
+
+  programs.starship.settings.palette = lib.mkForce "oceanic";
+  sops.age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
 }
