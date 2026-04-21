6b1bf248c21f11ecf59f2dc07b7efe89c4cda1ce
hcvst <hc@vst.io>
2026-04-21 16:18:50 +0200

    add starship themes

--- home/hcvst/uwshc.nix
+++ home/hcvst/uwshc.nix
@@ -4,5 +4,8 @@
     ./global
     ../common/optional/sops.nix
   ];
+
+  programs.starship.settings.palette = lib.mkForce "oceanic";
   sops.age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+
 }
