699ef86ed7aaec2bfe6acaa5f299e762f73c3aeb
hcvst <hc@vst.io>
2026-04-22 14:41:25 +0200

    enable sops for hcvst@ultkv

--- home/hcvst/ultkv.nix
+++ home/hcvst/ultkv.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ lib, config, ... }:
 {
   imports = [
     ./global
+    ../common/optional/sops.nix
+    ../common/optional/pet.nix
   ];
+
+  programs.starship.settings.palette = lib.mkForce "oceanic";
+  sops.age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+
 }
